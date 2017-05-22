790648ddfa4abf9966a84bca671f3de842727861
Krupiceva <dora.mlinaric94@gmail.com>
2017-05-22 09:41:41 +0200

    Dodani hexbin app i bin kod

--- LAB3/proj2/glavni.c
+++ LAB3/proj2/glavni.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <reg51.h>
 main(){
+	volatile unsigned char xdata *mem;
+	unsigned char vtest;
+	bit bdata test = 1;
 	TMOD=0x20;		/* Timer1:Gate|C/T|M1|M0| Timer0:Gate|C/T|M1|M0| */
 								/* Brojilo T1 radi na nacin rada 2. */
 	
@@ -14,6 +17,22 @@ main(){
 	SCON=0x52;		/* SM0|SM1|SM2|REN|TB8|RB8|TI|RI */
 								/* Serijsko sucelje radi na nacin rada 2. */
 	
-	printf("Zadatak 3 uspjesno napravljen");
+	printf("Zadatak 3 uspjesno napravljen.\n\n");
+	
+	/*4. zad */  
+	for(mem=0x0000; mem<=0x7FFF; mem++){
+		*mem = 0xAA;
+		vtest = *mem;
+		if(vtest != 0xAA){
+			test = 0;
+			break;
+		}
+	}
+	if(test == 1){
+		printf("Memorija je ispravna.\n\n");
+	}
+	else{
+		printf("Memorija nije ispravna.\n\n");
+	}
 	while(1){}
 }
